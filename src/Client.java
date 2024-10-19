26e04cc6fa8f4111395f81f6e05844ee592df30e
dlqja <beomjin754@gmail.com>
2024-10-19 18:24:57 +0900

    Feat: add student CRUD

--- src/Client.java
+++ src/Client.java
@@ -16,7 +16,7 @@ public class Client {
 		while (exit) {
 			try {
 				server = menuTui();
-				exit = userChoice(server, exit, reader);
+				userChoice(server,exit,reader);
 			} catch (MalformedURLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -26,31 +26,57 @@ public class Client {
 			} catch (NotBoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} catch (NullDataException e) {
+				
 			}
 		}
 	}
-	private static Boolean userChoice(ServerIF server, Boolean exit, BufferedReader reader)
-	        throws IOException, RemoteException {
+	private static void userChoice(ServerIF server, Boolean exit, BufferedReader reader)
+	        throws IOException, RemoteException, NullDataException {
 	    String sChoice = reader.readLine().trim();
-	    if (sChoice.equals("1")) {
-	        ArrayList<Student> students = server.getAllStudentData();
+	    switch(sChoice) {
+	    case "1":
 	        System.out.println("Server' answer:");
 	        System.out.println("sId        name    department  completedCoursesList ");
 	        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
-	        showList(students); // 통합된 출력 함수 호출
-	    } else if (sChoice.equals("2")) {
-	        ArrayList<Course> courses = server.getAllCourseData();
+	        showList(server.getAllStudentData()); // 통합된 출력 함수 호출
+	    	break;
+	    case "2":
 	        System.out.println("Server' answer:");
 	        System.out.println("cId        professor    cName         preRequisite ");
 	        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
-	        showList(courses); // 통합된 출력 함수 호출
-	    } else if (sChoice.equals("x")) {
+	        showList(server.getAllCourseData()); // 통합된 출력 함수 호출
+	        break;
+	    case "3":
+	    	addStudent(server, reader);
+	    	break;
+	    case "4":
+			deleteStudent(server, reader);
+			break;
+	    case "x":
 	        System.out.println("Process Stopped.");
 	        exit = false;
+	    default:
+	    	System.out.println("invalid choice!!!");
 	    }
-	    return exit;
 	}
-
+	
+	private static void addStudent(ServerIF server, BufferedReader reader) throws RemoteException, IOException{
+		System.out.println("------Student Information------");
+		System.out.println("Student ID: "); String studentId = reader.readLine().trim();
+		System.out.println("Student Name: "); String studentName = reader.readLine().trim();
+		System.out.println("Student Department: "); String studentDept = reader.readLine().trim();
+		System.out.println("Student Completed Course List: "); String completedCourse = reader.readLine().trim();
+		
+		if(server.addStudent(studentId+" "+studentName+" "+studentDept+" "+completedCourse))System.out.println("SUCCESS");
+		else System.out.println("FAIL");
+		}
+	
+	private static void deleteStudent(ServerIF server, BufferedReader reader) throws RemoteException, IOException {
+		System.out.print("Stident ID: ");
+	    if(server.deleteStudent(reader.readLine().trim()))System.out.println("SUCCESS");
+	    else System.out.println("FAIL");
+	}
 
 	private static ServerIF menuTui() throws NotBoundException, MalformedURLException, RemoteException {
 		ServerIF server;
@@ -61,6 +87,10 @@ public class Client {
 		System.out.println("ㅣ          1. List Students            ㅣ");
 		System.out.println("ㅣ                                      ㅣ");
 		System.out.println("ㅣ          2. List Courses             ㅣ");
+		System.out.println("ㅣ                                      ㅣ");
+		System.out.println("ㅣ          3. Add Student              ㅣ");
+		System.out.println("ㅣ                                      ㅣ");
+		System.out.println("ㅣ          4. Delete Student           ㅣ");
 		System.out.println("ㅣ                                      ㅣ");
 		System.out.println("ㅣ          Press 'x' to exit           ㅣ");
 		System.out.println("ㅣ                                      ㅣ");
